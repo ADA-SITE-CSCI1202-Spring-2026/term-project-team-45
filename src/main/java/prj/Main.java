f77fbdef51947f81267e65904d6487e213ad4cfb
Elmar <everdiyev22768@ada.edu.az>
2026-04-17 19:28:49 +0400

    Feature: Add Color texts to RadioZone class and implement OOP by moving GUI start up to Main class

--- src/main/java/prj/Main.java
+++ src/main/java/prj/Main.java
@@ -1,7 +1,10 @@
 package prj;
 
+import prj.gui.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Window window = new Window();
+        window.setVisible(true);
     }
 }
